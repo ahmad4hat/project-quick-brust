028ba14bb4e00bdb32bab3550da17c385b36cf23
Ahmad Farhat <rappar.stand@gmail.com>
2021-01-04 18:03:37 +0600

    sticky navbar added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,28 +2,38 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import Scss from "./header.module.scss"
 import NavList from "./Navigation/navigationList"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: "6rem",
-    }}
-  >
-    <nav className={Scss.Nav}>
-      <div className={Scss.NavBrand}>
-        <div className={Scss.NavBrandIcon}></div>
-        <Link to="/" className={Scss.NavBrandName}>
-          {siteTitle}
-        </Link>
-      </div>
+const Header = ({ siteTitle }) => {
+  const [scrolled, setScrolled] = useState(false)
+  const handleScroll = () => {
+    const offset = window.scrollY
+    if (offset > 40) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  })
+  return (
+    <header className={[Scss.Header, scrolled ? Scss.Sticky : ""].join(" ")}>
+      <nav className={Scss.Nav}>
+        <div className={Scss.NavBrand}>
+          <div className={Scss.NavBrandIcon}></div>
+          <Link to="/" className={Scss.NavBrandName}>
+            {siteTitle}
+          </Link>
+        </div>
 
-      <div className={Scss.NavMenu}>
-        <NavList />
-      </div>
-    </nav>
-  </header>
-)
+        <div className={Scss.NavMenu}>
+          <NavList />
+        </div>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
