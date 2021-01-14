c8590c81fcc964462ebeab56402117a9387fd9af
Ahmad Farhat <rappar.stand@gmail.com>
2021-01-14 08:47:10 +0600

    basic stucture of the team page complete

---
+++ src/components/Member/Member.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Scss from "./Member.module.scss"
+import Img from "gatsby-image"
+
+const Member = props => {
+  console.log(props.role)
+  return (
+    <div className={Scss.Card}>
+      <div className={Scss.ImgContainer}>
+        <Img className={Scss.Img} fluid={props.image}></Img>
+      </div>
+      <h2 className={Scss.Name}>{props.name}</h2>
+      <h4>{props.role}</h4>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+Member.propTypes = {
+  image: PropTypes.object,
+  name: PropTypes.string,
+  role: PropTypes.string,
+  description: PropTypes.string,
+}
+
+export default Member
--- src/containers/Team/Team.js
+++ src/containers/Team/Team.js
@@ -2,9 +2,10 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import Scss from "./Team.module.scss"
-import Img from "gatsby-image"
+import Member from "./../../components/Member/Member"
+import { motion } from "framer-motion"
 const Team = props => {
-  const ahmad = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     {
       file(relativePath: { eq: "farhat.png" }) {
         childImageSharp {
@@ -13,16 +14,46 @@ const Team = props => {
           }
         }
       }
+      placeholder: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(grayscale: true) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
+  const description =
+    "Trying to Solve the front end and and consumer facing problem "
+
   return (
-    <div className={Scss.Container}>
-      <h1>Hello</h1>
+    <motion.div
+      initial={{ x: "-20%" }}
+      animate={{ x: 0 }}
+      className={Scss.Container}
+    >
       {/* <img src={ahmad.c}> </img> */}
       {/* <div className={Scss.Img}> */}
-      <Img className={Scss.Img} fluid={ahmad.file.childImageSharp.fluid}></Img>
+      <Member
+        image={data.file.childImageSharp.fluid}
+        name={"Ahmad, Farhat"}
+        role={"Frontend and UI / UX"}
+        description={description}
+      ></Member>
+      <Member
+        image={data.placeholder.childImageSharp.fluid}
+        name={"Placeholder"}
+        role={"Test"}
+        description={description}
+      ></Member>
+      <Member
+        image={data.placeholder.childImageSharp.fluid}
+        name={"Placeholder"}
+        role={"Test"}
+        description={description}
+      ></Member>
       {/* </div> */}
-    </div>
+    </motion.div>
   )
 }
 
